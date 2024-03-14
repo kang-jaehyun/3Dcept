d245e851ee1bbba73e93d9a77d3526d5667e0610
kang-jaehyun <jaehyunkang@yonsei.ac.kr>
2024-03-14 14:36:33 +0900

    add weightedloss

--- pointcept/models/losses/misc.py
+++ pointcept/models/losses/misc.py
@@ -38,6 +38,33 @@ class CrossEntropyLoss(nn.Module):
     def forward(self, pred, target):
         return self.loss(pred, target) * self.loss_weight
 
+@LOSSES.register_module()
+class BalancedCrossEntropyLoss(nn.Module):
+    def __init__(
+        self,
+        weight=None,
+        size_average=None,
+        reduce=None,
+        reduction="mean",
+        label_smoothing=0.0,
+        loss_weight=1.0,
+        ignore_index=-1,
+    ):
+        super(CrossEntropyLoss, self).__init__()
+        weight = torch.tensor(weight).cuda() if weight is not None else None
+        self.loss_weight = loss_weight
+        self.loss = nn.CrossEntropyLoss(
+            weight=weight,
+            size_average=size_average,
+            ignore_index=ignore_index,
+            reduce=reduce,
+            reduction=reduction,
+            label_smoothing=label_smoothing,
+        )
+
+    def forward(self, pred, target):
+        return self.loss(pred, target) * self.loss_weight
+
 
 @LOSSES.register_module()
 class SmoothCELoss(nn.Module):
