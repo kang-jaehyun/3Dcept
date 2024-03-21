62ac63dca246ca7aa73dc358c20e3734654de00b
kang-jaehyun <jaehyunkang@yonsei.ac.kr>
2024-03-22 02:25:45 +0900

    edit arguments for better usage

--- run_gen_all.sh
+++ run_gen_all.sh
@@ -2,22 +2,36 @@
 SCRIPT_DIR=$(dirname "$0")
 echo [SCRIPT_DIR] $SCRIPT_DIR
 
-export num_gpus=4
+export num_gpus=2
 export num_nodes=1
 export MASTER_PORT=12359
 
+slurm_suma_3090_big="-p big_suma_rtx3090 -q big_qos"
+slurm_dell_3090_base="-p dell_rtx3090"
+slurm_dell_3090_big="-p dell_rtx3090 -q big_qos"
+slurm_suma_a6000_base="-p suma_a6000"
+slurm_suma_a6000_big="-p suma_a6000 -q big_qos"
+slurm_a100="-p suma_a100 -q a100_qos"
+export SLURM_SETTING=$slurm_dell_3090_big
+
 DURATION='3-0:0:0'
-slurm_setting="-p base -q base_qos"
-slurm_setting2="-p big -q big_qos"
-slurm_setting_a100="-p suma_a100 -q a100_qos"
 curr_date="$(date +'%m/%d-%H:%M:%S')"
 JNAME="ptv3-g${num_gpus}n${num_nodes}-${curr_date}" 
 echo "[MASTER] JNAME: ${JNAME} | DURATION: ${DURATION} | num_gpus: ${num_gpus}"
 
 ### Run master node
-export DETECTRON2_DATASETS=/datasets
+export CONFIG_FILE=" -d scannetpp -c semseg-pt-v3m1-0-base-top100 -n ptv3_aidc "
 export node_idx=0
-sbatch --gres=gpu:$num_gpus --cpus-per-task=8 $slurm_setting_a100 -J $JNAME --time=$DURATION ./${SCRIPT_DIR}/run_gen_master_node.sh
+export mnt=" -B /share0/jhkang:/share0/jhkang "
+export docker=" /share0/jhkang/simg/3d_20.simg "
+
+# export exec_file=" sh scripts/train.sh "
+export exec_file=" sh scripts/test.sh -w model_best"
+
+export dataloader_cpu=" -o 1 "
+export dist_url=" -t ${DIST_URL} " 
+
+sbatch --gres=gpu:$num_gpus --cpus-per-task=8 $SLURM_SETTING -J $JNAME --time=$DURATION ./${SCRIPT_DIR}/run_gen_master_node.sh
 sleep 10
 
 ### get master node address
@@ -46,6 +60,6 @@ for i in $(seq 1 $(($num_nodes-1)))
 do
   echo "node $i"
   export node_idx=$i
-  sbatch --gres=gpu:$num_gpus --cpus-per-task=8 $slurm_setting_a100 -J $JNAME --time=$DURATION ./${SCRIPT_DIR}/run_gen_slave_node.sh
+  sbatch --gres=gpu:$num_gpus --cpus-per-task=8 $SLURM_SETTING -J $JNAME --time=$DURATION ./${SCRIPT_DIR}/run_gen_slave_node.sh
 done
 
--- run_gen_master_node.sh
+++ run_gen_master_node.sh
@@ -10,15 +10,8 @@ MASTER_NODE=$SLURM_NODELIST # sbatch output environment 사용
 DIST_URL="tcp://$MASTER_NODE:$MASTER_PORT"
 echo "num_nodes: ${num_nodes} | current node: ${node_idx}"
 echo "num_gpus: $num_gpus | DIST_URL: ${DIST_URL}"
-
-mnt="-B /share0/jhkang:/share0/jhkang "
-docker=" /share0/jhkang/simg/3d.simg "
-exec_file=" sh scripts/train.sh "
-machine_cfg=" -g ${num_gpus} -m ${num_nodes} -k ${node_idx}"
-config_file=" -d scannetpp -c semseg-pt-v3m1-0-base-top100 -n ptv3_aidc "
-dataloader_cpu=" -o 1"
-dist_url="-t ${DIST_URL}" 
+machine_cfg=" -g ${num_gpus} -m ${num_nodes} -k ${node_idx} "
 
 ml purge
 ml load singularity
-singularity exec --nv $mnt $docker $exec_file $config_file $machine_cfg $dist_url $dataloader_cpu
+singularity exec --nv $mnt $docker $exec_file $CONFIG_FILE $machine_cfg $dist_url $dataloader_cpu
